4b15e96575a21a5f46429b7482439b19e36abb3a
Ícaro Agostino <icaroagostino@gmail.com>
2020-03-24 22:10:59 +0100

    Update Monty_Hall.R

--- Monty_Hall.R
+++ Monty_Hall.R
@@ -1,100 +1,88 @@
-library(simmer)
-library(simmer.plot)
+library(ggplot2)
 
-n_run <- 30     # Números de replicação para cada simulação
-sim_t <- 960*7  # 7 dias (2 turnos)
+# Demonstracao do Problema de Monty Hall
 
-# Criando uma funcao para simular um cenario
-simular <- function(Punching,
-                    Bending,
-                    Welding,
-                    Pressing,
-                    Drilling){
-  set.seed(123)
-  # Parametros a serem variados na simulacao
-  Punching <- round(Punching, 0)
-  Bending  <- round(Bending,  0)
-  Welding  <- round(Welding,  0)
-  Pressing <- round(Pressing, 0)
-  Drilling <- round(Drilling, 0)
-  # Criando ambiente de simulacao
-  env <- simmer("model")
-  # Definindo a trajetoria
-  flowShop <- trajectory("flowShop") %>%
-    ## add a Punching activity 
-    seize("Punching", 1) %>%
-    timeout(function() rnorm(1, 10)) %>%
-    release("Punching", 1) %>%
-    ## add a Bending activity
-    seize("Bending", 1) %>%
-    timeout(function() rnorm(1, 20)) %>%
-    release("Bending", 1) %>%
-    ## add a Welding activity
-    seize("Welding", 1) %>%
-    timeout(function() rnorm(1, 15)) %>%
-    release("Welding", 1) %>%
-    ## add a Pressing activity
-    seize("Pressing", 1) %>%
-    timeout(function() rnorm(1, 12)) %>%
-    release("Pressing", 1) %>%
-    ## add a Drilling activity
-    seize("Drilling", 1) %>%
-    timeout(function() rnorm(1, 6)) %>%
-    release("Drilling", 1)
-  # Adcionando recursos
-  env %>%
-    add_resource("Punching", Punching) %>%
-    add_resource("Bending",  Bending ) %>%
-    add_resource("Welding",  Welding ) %>%
-    add_resource("Pressing", Pressing) %>%
-    add_resource("Drilling", Drilling) %>%
-    add_generator("flowShop", flowShop, function() rexp(1, 1/5))
-  # Run com replicacoes
-  envs <<- lapply(1:n_run, function(i) {
-    reset(env) ; run(env, sim_t) # Simula tempo definido em sim_t
-  })
-  # Calculando a utilizacao media dos recursos
-  util <- get_mon_resources(envs) %>%
-    dplyr::group_by(.data$resource, .data$replication) %>%
-    dplyr::mutate(dt = .data$time - dplyr::lag(.data$time)) %>%
-    dplyr::mutate(in_use = .data$dt * dplyr::lag(.data$server / .data$capacity)) %>%
-    dplyr::summarise(utilization = sum(.data$in_use, na.rm = TRUE) / sum(.data$dt, na.rm=TRUE)) %>%
-    dplyr::summarise(utilization = mean(.data$utilization))
-  # Retorna a utilização média dos recursos
-  return(util[[2]] %>% mean)
+n <- 10000 # Numero de repeticoes
+vitorias <- c() # Vetor onde serao gravadas as vitorias
+portas <- c(1:3) # Vetor de portas
+
+###################################
+# Estrategia 1 - Nao muda a porta #
+###################################
+
+for (i in 1:n){
+  
+  porta_premio <- sample(x = portas, size = 1) # porta com premio
+  escolha <- sample(x = portas, size = 1) # escolha do jogador
+  
+  # Se a escolha for igual a porta premiada vence [1]
+  if (porta_premio == escolha){
+    
+    vitorias <- c(vitorias, 1)
+    
+  # Se a escolha for diferente da porta premiada perde [0]
+  } else {
+    
+    vitorias <- c(vitorias, 0)
+    
+  }
 }
 
-library(GA)
-library(parallel)
+# Computa resultados
+data <-
+  data.frame(rodada = c(1:n),
+             perc.ganha = cumsum(vitorias)/c(1:n))
+  
+# Plot
+resultados_estrategia1 <- 
+  ggplot(data, aes(x = rodada, y = perc.ganha)) +
+  geom_line() +
+  geom_hline(yintercept=1/3,
+             color = "red")
 
-popSize <- 30         # Tamanho da população
-maxiter <- 500        # Max de iteração
-run <- 10             # Numero de iterações iguais que para otimização
-pcrossover <- 0.8     # Crossover
-pmutation <- 0.1      # Mutação
-parallel <- TRUE      # Parelizando a avalição dos individuos
-seed <- 123
+resultados_estrategia1
 
-lower <- c(1,1,1,1,1) # Restrições de max
-upper <- c(5,5,5,5,5) # Restrições de min
+###############################
+# Estrategia 2 - Muda a porta #
+###############################
 
-# Otimizacao em GA para encontrar a combinacao otima de recursos
-GA <- ga(type = "real-valued",
-         fitness = function(x) simular(x[1],x[2],x[3],x[4],x[5]),
-         lower = lower,
-         upper = upper,
-         popSize = popSize,
-         maxiter = maxiter,
-         run = run,
-         pcrossover = pcrossover,
-         pmutation = pmutation,
-         parallel = parallel,
-         seed = seed)
+vitorias <- c() # Vetor onde serao gravadas as vitorias
 
-# Melhor solucao
-solution <- round(GA@solution) %>% print
-plot(GA) # Plota busca
+for (i in 1:n){
+  
+  porta_premio <- sample(x = portas, size = 1) # porta com premio
+  escolha1 <- sample(x = portas, size = 1) # primeira escolha do jogador
+  
+  x <- portas[-c(porta_premio, escolha1)] # variavel auxliar
+  
+  porta_eliminada <- if(length(x) > 1) sample(x, size = 1) else x # elimina
+  
+  escolha2 <- portas[-c(escolha1,porta_eliminada)] # jogador muda de porta
+  
+  # Se a escolha 2 for igual a porta premiada vence [1]
+  if (porta_premio == escolha2){
+    
+    vitorias <- c(vitorias, 1)
+    
+  # Se a escolha 2 for diferente da porta premiada perde [0]
+  } else {
+    
+    vitorias <- c(vitorias, 0)
+    
+  }
+  
+}
 
-# Simula melhor solucao
-do.call(simular, as.list(solution))
-plot(get_mon_resources(envs)) # Plota resultados
+# Computa resultados
+data <-
+  data.frame(rodada = c(1:n),
+             perc.ganha = cumsum(vitorias)/c(1:n))
+
+# Plot
+resultados_estrategia2 <-
+  ggplot(data, aes(x = rodada, y = perc.ganha)) +
+  geom_line() +
+  geom_hline(yintercept=2/3,
+             color = "red")
+
+resultados_estrategia2
