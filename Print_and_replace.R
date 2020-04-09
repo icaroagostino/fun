3a453d850f377a40bc2d6e71689cfea639936c61
Ícaro Agostino <icaroagostino@gmail.com>
2020-04-10 00:52:07 +0200

    Update Print_and_replace.R

--- Print_and_replace.R
+++ Print_and_replace.R
@@ -1,4 +1,6 @@
-for (i in seq_len(10000)){
-  cat('\r', i)
-  Sys.sleep(0.1)
-}
+for (i in seq_len(10)){
+  
+  cat('\r', 'Você têm',10-i,'segs')
+  Sys.sleep(1)
+  
+}; cat('\r', ' Você morreu :/  ')
